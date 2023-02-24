ab8914c33e6745171b81ecb8efae634ee9d6b10b
Jonathan Gao <gsmlg.com@gmail.com>
2023-02-24 15:34:54 +0800

    fix: Fix test.

--- test/caddy_server_test.exs
+++ test/caddy_server_test.exs
@@ -4,6 +4,6 @@ defmodule CaddyServerTest do
 
   test "test caddy download url" do
     assert CaddyServer.download_url() =~
-             "https://github.com/caddyserver/caddy/releases/download/v2.6.4/caddy_2.6.4_mac_arm64.tar.gz"
+             "https://github.com/caddyserver/caddy/releases/download/v2.6.4/caddy_2.6.4_"
   end
 end
