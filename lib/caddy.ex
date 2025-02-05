fa02e4fd5cd8b380498b2d2191acd02c41f96e84
Jonathan Gao <gsmlg.com@gmail.com>
2025-02-05 18:25:19 +0800

    chore: Format code.

--- lib/caddy.ex
+++ lib/caddy.ex
@@ -5,7 +5,37 @@ defmodule Caddy do
 
   Start Caddy HTTP Server in supervisor tree
 
+  - Start in your application
 
+  ```elixir
+  def start(_type, _args) do
+    children = [
+      # Start a Caddy by calling: Caddy.start_link([])
+      {Caddy, [
+        caddy_bin: "/usr/bin/caddy",
+      ]}
+      # Start the Telemetry supervisor
+      PhoenixWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: PhoenixWeb.PubSub},
+      # Start the Endpoint (http/https)
+      PhoenixWeb.Endpoint
+    ]
+
+    opts = [strategy: :one_for_one, name: PhoenixWeb.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+  ```
+
+  - Start in extra_applications
+
+  ```elixir
+  def application do
+    [
+      extra_applications: [Caddy.Application]
+    ]
+  end
+  ```
 
   """
   require Logger
